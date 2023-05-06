94bb1ae15e9153a627782cc4a47da11433c4abae
username <Jzazzi56@gmail.comn>
2023-05-06 15:09:39 -0700

    Third commit adding setTimeouts to buttons

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
   const scoreBoard = document.querySelector('.score');
   const jeapordyquestion = document.querySelector('.jeapordyquestion')
   const jeapordyanswer =  document.querySelector('.jeapordyanswer')
-  console.log(jeapordyquestion)
+  
   
 //row 100
   const button1 = document.querySelector("#Btn1-100");
@@ -52,166 +52,216 @@
   button1.addEventListener('click', async (event) => {
       const response = await fetch('https://jservice.io/api/category?id=102');
       const data1 = await response.json()
-      console.log(data1)
       jeapordyquestion.innerHTML = `${data1.clues[0, 1, 2, 3, 4, 5].question}`;
       setTimeout(() => {
         jeapordyanswer.innerHTML = `${data1.clues[0, 1, 2 , 3, 4, 5].answer}`;
-      }, 15000) 
-
-      
+      }, 15000);   
       
   });
   button6.addEventListener('click', async (event) => {
       const response = await fetch('https://jservice.io/api/category?id=102');
       const data1 = await response.json()
-      console.log(data1)
       jeapordyquestion.innerHTML = `${data1.clues[6, 7, 8, 9, 10].question}`;
-  });
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data1.clues[6, 7, 8, 9, 10].answer}`;
+      }, 15000);
+        
+    });     
   button11.addEventListener('click', async (event) => {
       const response = await fetch('https://jservice.io/api/category?id=102');
       const data1 = await response.json()
-      console.log(data1)
       jeapordyquestion.innerHTML = `${data1.clues[11, 12, 13, 14, 15].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data1.clues[11, 12 , 13, 14, 15].answer}`;
+      }, 15000)
+      
   });
   button16.addEventListener('click', async (event) => {
       const response = await fetch('https://jservice.io/api/category?id=102');
       const data1 = await response.json()
-      console.log(data1)
       jeapordyquestion.innerHTML = `${data1.clues[16, 17, 18, 19, 20].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data1.clues[16, 17, 18, 19, 20].answer}`;
+      }, 15000)
+      
   });
   button21.addEventListener('click', async (event) => {
       const response = await fetch('https://jservice.io/api/category?id=102');
       const data1 = await response.json()
       console.log(data1)
       jeapordyquestion.innerHTML = `${data1.clues[21, 22, 23, 24, 25].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data1.clues[21, 22 , 23, 24, 25].answer}`;
+      }, 15000)
+      
   });
 
   //Songs category
     button2.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=1242");
       const data2 = await response.json()
-      console.log(data2)
       jeapordyquestion.innerHTML = `${data2.clues[0, 1, 2, 3, 4].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data2.clues[0, 1, 2 , 3, 4].answer}`;
+      }, 15000)
   });
   button7.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=1242");
       const data2 = await response.json()
-      console.log(data2)
       jeapordyquestion.innerHTML = `${data2.clues[5, 6, 7, 8, 9].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data2.clues[5, 6, 7, 8, 9].answer}`;
+      }, 15000)
   });
   button12.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=1242");
       const data2 = await response.json()
-      console.log(data2)
       jeapordyquestion.innerHTML = `${data2.clues[10, 11, 12, 13, 14].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data2.clues[11, 12 , 13, 14].answer}`;
+      }, 15000)
   });
   button17.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=1242");
       const data2 = await response.json()
-      console.log(data2)
       jeapordyquestion.innerHTML = `${data2.clues[15, 16, 17, 18, 19].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data2.clues[15, 16, 17, 18, 19].answer}`;
+      }, 15000)
   });
   button22.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=1242");
       const data2 = await response.json()
-      console.log(data2)
       jeapordyquestion.innerHTML = `${data2.clues[20, 21, 22, 23, 24].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data2.clues[21, 22 , 23, 24].answer}`;
+      }, 15000)
   });
 
   //Food category
   button3.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=122");
       const data3 = await response.json()
-      console.log(data3)
       jeapordyquestion.innerHTML = `${data3.clues[0, 1, 2 ,3 ,4].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data3.clues[0, 1, 2, 3, 4].answer}`;
+      }, 15000)
   });
   button8.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=122");
       const data3 = await response.json()
-      console.log(data3)
       jeapordyquestion.innerHTML = `${data3.clues[5, 6, 7, 8, 9].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data3.clues[5, 6, 7, 8, 9].answer}`;
+      }, 15000)
   });
   button13.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=122");
       const data3 = await response.json()
-      console.log(data3)
       jeapordyquestion.innerHTML = `${data3.clues[10, 11, 12, 13, 14].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data3.clues[10, 11, 12, 13, 14].answer}`;
+      }, 15000)
   });
   button18.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=122");
       const data3 = await response.json()
-      console.log(data3)
       jeapordyquestion.innerHTML = `${data3.clues[15, 16, 17, 18, 19].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data3.clues[15, 16, 17, 18, 19].answer}`;
+      }, 15000)
   });
   button23.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=122");
       const data3 = await response.json()
-      console.log(data3)
       jeapordyquestion.innerHTML = `${data3.clues[20, 21, 22, 23, 24].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data3.clues[20, 21, 22, 23, 24].answer}`;
+      }, 15000)
   });
 
   //Dog category
   button4.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=371");
       const data4 = await response.json()
-      console.log(data4)
       jeapordyquestion.innerHTML = `${data4.clues[0, 1, 2, 3, 4].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data4.clues[0, 1, 2, 3, 4].answer}`;
+      }, 15000)
   });
   button9.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=371");
       const data4 = await response.json()
-      console.log(data4)
       jeapordyquestion.innerHTML = `${data4.clues[5, 6, 7, 8, 9].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data4.clues[5, 6, 7, 8, 9].answer}`;
+      }, 15000)
   });
   button14.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=371");
       const data4 = await response.json()
-      console.log(data4)
       jeapordyquestion.innerHTML = `${data4.clues[10, 11, 12, 13, 14].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data4.clues[10, 11, 12, 13, 14].answer}`;
+      }, 15000)
   });
   button19.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=371");
       const data4 = await response.json()
-      console.log(data4)
       jeapordyquestion.innerHTML = `${data4.clues[15, 16, 17, 18, 19].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data4.clues[15, 16, 17, 18, 19].answer}`;
+      }, 15000)
   });
   button24.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=371");
       const data4 = await response.json()
-      console.log(data4)
       jeapordyquestion.innerHTML = `${data4.clues[20, 21, 22, 23, 24].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data4.clues[20, 21, 22, 23, 24].answer}`;
+      }, 15000)
   });
 
   //Fwords category
   button5.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=458");
       const data5 = await response.json()
-      console.log(data5)
       jeapordyquestion.innerHTML = `${data5.clues[0, 1, 2].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data5.clues[0, 1, 2].answer}`;
+      }, 15000)
   });
   button10.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=458");
       const data5 = await response.json()
-      console.log(data5)
       jeapordyquestion.innerHTML = `${data5.clues[4, 5, 6].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data5.clues[4, 5, 6].answer}`;
+      }, 15000)
   });
   button15.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=458");
       const data5 = await response.json()
-      console.log(data5)
       jeapordyquestion.innerHTML = `${data5.clues[7, 8, 9].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data5.clues[7, 8, 9].answer}`;
+      }, 15000)
   });
   button20.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=458");
       const data5 = await response.json()
-      console.log(data5)
       jeapordyquestion.innerHTML = `${data5.clues[10, 11, 12].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data5.clues[10, 11, 12].answer}`;
+      }, 15000)
   });
   button25.addEventListener('click', async (event) => {
       const response = await fetch("https://jservice.io/api/category?id=458");
       const data5 = await response.json()
-      console.log(data5)
       jeapordyquestion.innerHTML = `${data5.clues[13, 14, 15].question}`;
+      setTimeout(() => {
+        jeapordyanswer.innerHTML = `${data5.clues[13, 14, 15].answer}`;
+      }, 15000)
   });
   
       /*----- functions -----*/
